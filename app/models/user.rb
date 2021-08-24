c9cd51202b471e5e8c6bddde7e6808295869c9be
Konstantin <tarasov.1k@yandex.ru>
2021-08-24 22:26:13 +0500

    added validation, book model, and association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true,
+            length: {minimum: 2},
+            uniqueness: true
+
+  validates :email, presence: true,
+            uniqueness: true
+
+  has_many :books
 end
