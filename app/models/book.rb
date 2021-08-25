5b570c8773b3f2e9cbfa90841fdc5cd158f176f9
Konstantin <tarasov.1k@yandex.ru>
2021-08-25 11:53:11 +0500

    book validation added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 255}
+  validates :description, presence: true
+  validates :year, presence: true
+  validates :author, presence: true
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true,
-            length: {minimum: 2},
-            uniqueness: true
-
-  validates :email, presence: true,
-            uniqueness: true
+  validates :name, presence: true, length: {minimum: 2}, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   has_many :books
 end
